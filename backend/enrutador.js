9fc7bdaca943e5ec7929a9f67a17b6d3c37e7e61
Lautaro <lautarorousseau@gmail.com>
2021-06-22 07:27:57 -0300

    M

--- backend/enrutador.js
+++ backend/enrutador.js
@@ -5,14 +5,14 @@ const propietarios = require("./rutas/propietarios");
 const consultas = require("./rutas/consultas");
 
 module.exports = {
-    ruta: (data, callback) => { //creamos la ruta y el handler(es el manejador de la ruta)
-        callback(200, {mensaje: "esta es /ruta"});
-    },
-    autos: autos(recursos.autos), //ejecutamos la funcion de autos, ejecutando recursos
-    mecanicos: mecanicos(recursos.mecanicos),
-    propietarios: propietarios(recursos.propietarios), 
-    consultas: consultas(recursos), 
-    noEncontrado: (data, callback) => {
-        callback (404, { mensaje: "no encontrado" });
-    },
+  ruta: (data, callback) => {
+    callback(200, { mensaje: "esta es /ruta" });
+  },
+  autos: autos(recursos.autos),
+  mecanicos: mecanicos(recursos.mecanicos),
+  propietarios: propietarios(recursos.propietarios),
+  consultas: consultas(recursos),
+  noEncontrado: (data, callback) => {
+    callback(404, { mensaje: "no encontrado" });
+  },
 };
